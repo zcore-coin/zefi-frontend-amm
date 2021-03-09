9d0d1d6a8c15fc10b4175e6b01f5635ab75a9399
Mosqueiro <35813800+zcore-coin@users.noreply.github.com>
2021-03-09 00:39:41 -0300

    First release

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x0288D3E353fE2299F11eA2c2e1696b4A648eCC07',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x24b87c29e907d6329ed8fd6d7b2ecb074089093e'
+}
+
+export default priceContracts
